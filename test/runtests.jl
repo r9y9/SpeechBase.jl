80a463d9a304156f94dec78c8b4396bb12e48ae7
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2015-01-01 23:52:57 +0900

    Add very simple tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,3 +4,18 @@ using Base.Test
 for fname in ["stft"]
     include(string(fname, ".jl"))
 end
+
+function test_excitation()
+    println("Testing: excitation signal generation")
+    f0 = [zeros(100), 100*ones(100), zeros(100)]
+    fs = 16000
+    hopsize = 80
+
+    e = generate(UniformExcitation(), f0, fs, hopsize)
+    @test !any(isnan(e))
+
+    e = generate(GaussExcitation(), f0, fs, hopsize)
+    @test !any(isnan(e))
+end
+
+test_excitation()

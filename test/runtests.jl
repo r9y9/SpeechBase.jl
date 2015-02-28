357360695fda1379f02231b97aa451a6982e19d3
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2015-02-28 15:33:30 +0900

    merge stft.jl into runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,41 @@
 using SpeechBase
+using WAV
 using Base.Test
 
-for fname in ["stft"]
-    include(string(fname, ".jl"))
+# load test data
+x_test16k, fs = wavread("test16k.wav")
+x_test16k = vec(x_test16k)
+@assert fs == 16000
+
+let
+    println("test consistency between stft and istft")
+
+    x = copy(x_test16k)
+    for winfunc in (hanning, blackman)
+        for framelen in (4096, 2048, 1024, 512)
+            const win = winfunc(framelen)
+            for shiftdenom in (2, 3, 4, 5, 6, 7, 8) # 50% overlap, 75% ...
+                const hopsize = div(framelen, shiftdenom)
+
+                X = stft(x, framelen, hopsize, win)
+                @test !any(isnan(X))
+
+                y = istft(X, framelen, hopsize, win)
+                @test !any(isnan(y))
+
+                len = min(length(x), length(y))
+                x, y = x[1:len], y[1:len]
+
+                # check reconstruction error
+                err = norm(x-y)/norm(x)
+                @show (framelen, hopsize, winfunc, err)
+                @test norm(x-y)/norm(x) < 0.05
+            end
+        end
+    end
 end
 
-function test_excitation()
+let
     println("Testing: excitation signal generation")
     f0 = [zeros(100), 100*ones(100), zeros(100)]
     fs = 16000
@@ -17,5 +47,3 @@ function test_excitation()
     e = generate(GaussExcitation(), f0, fs, hopsize)
     @test !any(isnan(e))
 end
-
-test_excitation()
